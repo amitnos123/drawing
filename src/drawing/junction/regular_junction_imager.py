dffecd122462001a2d39f40c29dee1b2d60f75b4
Amit Sami Nosatzki <sami.nosatzki@gmail.com>
2025-08-24 11:41:03 +0300

    created regular junction imager

---
+++ src/drawing/junction/regular_junction_imager.py
@@ -0,0 +1,33 @@
+from .base_junction_imager import BaseJunctionImager
+import gdsfactory as gf
+
+class RegularJunctionImager(BaseJunctionImager):
+    length: float = 140
+    width: float = 93
+
+    # 140 width
+    # 93 height 
+
+    # 100x microscope window
+    
+    def build(self) -> gf.Component:
+        return RegularJunctionImager.regularJunctionImager(
+        self.length, 
+        self.width, 
+        self.layer
+    )
+
+    
+    @gf.cell
+    @staticmethod
+    def regularJunctionImager(length: float, width: float, layer: gf.typings.LayerSpec) -> gf.Component:
+        c = gf.Component()
+        c << gf.components.rectangle(size=(length, width), layer=layer)
+        return c
+    
+    def validate(self) -> None:
+        super().validate()
+        if self.length <= 0:
+            raise ValueError("Length must be positive.")
+        if self.width <= 0:
+            raise ValueError("Width must be positive.")
