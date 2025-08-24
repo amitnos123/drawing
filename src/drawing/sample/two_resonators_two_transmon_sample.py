c76b8dca9741701f224153c123d0fac1482e4b89
Amit Sami Nosatzki <sami.nosatzki@gmail.com>
2025-08-24 16:57:50 +0300

    basic implementation of sample

---
+++ src/drawing/sample/two_resonators_two_transmon_sample.py
@@ -0,0 +1,56 @@
+from drawing.junction.base_junction import BaseJunctionConfig
+from drawing.meander.euler import meander_euler
+from drawing.resonator.base_resonator import BaseResonator
+from drawing.resonator.meander_resonator import MeanderResonator
+from drawing.transmon.transmon import TransmonConfig
+from .base_sample import BaseSampleConfig
+import gdsfactory as gf
+
+class twoResonatorsTwoTransmonSampleConfig(BaseSampleConfig):
+
+    transmon : TransmonConfig = TransmonConfig()
+    transmonAntenna : TransmonConfig = TransmonConfig()
+
+    ResonatorLeft : BaseResonator = MeanderResonator()
+    ResonatorRight : BaseResonator = MeanderResonator()
+
+    def build(self) -> gf.Component:
+        return twoResonatorsTwoTransmonSampleConfig.twoResonatorsTwoTransmonSample(
+            transmon=self.transmon.build(),
+            transmonAntenna=self.transmonAntenna.build(),
+            ResonatorLeft=self.ResonatorLeft.build(),
+            ResonatorRight=self.ResonatorRight.build()
+        )
+
+    @staticmethod
+    @gf.cell
+    def twoResonatorsTwoTransmonSample(transmon : gf.Component, transmonAntenna : gf.Component, ResonatorLeft : gf.Component, ResonatorRight : gf.Component) -> gf.Component:
+        c = gf.Component()
+
+        res = meander_euler(wire_width = 100,
+        height = 1025,
+        padding_length = 2400,
+        spacing = 300,
+        num_turns = 6,
+        radius = 100)
+
+        res_left_ref = c << res.copy()
+        res_right_ref = c << res.copy()
+
+        # res_left_ref = c << ResonatorLeft
+        # res_right_ref = c << ResonatorRight
+
+        res_right_ref.movex(res_left_ref.xmax - res_right_ref.xmin + 3500)
+
+        transmon_ref = c << transmon
+        transmon_antenna_ref = c << transmonAntenna
+
+        transmon_ref.movex(res_right_ref.xmax - transmon_ref.xmin + 400)
+
+        transmon_antenna_ref.movex(transmon_ref.xmax - transmon_antenna_ref.xmin + 300)
+
+        return c
+
+
+    def get_jopherson_junctions(self) -> list[BaseJunctionConfig]:
+        return [self.transmon.junction, self.transmonAntenna.junction]
