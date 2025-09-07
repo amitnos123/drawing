c27134c7a3b49852902befec888d62ec110f953b
Amit Sami Nosatzki <sami.nosatzki@gmail.com>
2025-09-07 16:54:28 +0300

    added Resistance CSV

--- create_directory.py
+++ create_directory.py
@@ -3,7 +3,108 @@ import gdsfactory as gf
 import json
 
 def create_wafer_dir(gds: gf.Component, dir_name: str, design_json: str) -> None:
-    
+    """
+    Creates a nested directory structure for wafer design files and saves the GDS file and design JSON.
+    Args:
+        gds (gf.Component): The GDS component to be saved.
+        dir_name (str): The name of the main directory to be created.
+        design_json (str): The design JSON content to be saved in the Design directory.
+    example for design_json:
+    {
+  "wafer name": "20250907_",
+  "material": "Silicon",
+  "size": 76200,
+  "thickness": 1,
+  "recipe": "NORMAL",
+  "samples": [
+    {
+      "name": "Sample1",
+      "dimension": [
+        15000,
+        5000
+      ],
+      "center": [
+        0,
+        0
+      ],
+      "jopherson junctions": [
+        {
+          "type": "DOLAN",
+          "gap": 1,
+          "name": "A"
+        },
+        {
+          "type": "DOLAN",
+          "gap": 1,
+          "name": "B"
+        }
+      ],
+      "resistance measurement points": [
+        {
+          "point": "(0,0)",
+          "name": "A"
+        },
+        {
+          "point": "(1,1)",
+          "name": "B"
+        }
+      ]
+    },
+    {
+      "name": "Sample2",
+      "dimension": [
+        15000,
+        5000
+      ],
+      "center": [
+        0,
+        0
+      ],
+      "jopherson junctions": [
+        {
+          "type": "DOLAN",
+          "gap": 1,
+          "name": "A"
+        },
+        {
+          "type": "DOLAN",
+          "gap": 1,
+          "name": "B"
+        }
+      ],
+      "resistance measurement points": [
+        {
+          "point": "(0,0)",
+          "name": "A"
+        },
+        {
+          "point": "(1,1)",
+          "name": "B"
+        }
+      ]
+    }
+  ],
+  "test junctions": [
+    {
+      "type": "DOLAN",
+      "BRIDGE START GAP": 1,
+      "BRIDGE END GAP": 5,
+      "LEFT FINGER": {
+        "layer": "(1, 0)",
+        "length": 10,
+        "width": 1
+      },
+      "RIGHT FINGER": {
+        "layer": "(1, 0)",
+        "length": 10,
+        "width": 1
+      }
+    }
+  ]
+}
+    """
+
+
     # Specify the nested directory structure
     main_dir_path = Path("I:\\SergeR_Group\\Notes\\Fabrication\\" + dir_name)
 
@@ -19,16 +120,29 @@ def create_wafer_dir(gds: gf.Component, dir_name: str, design_json: str) -> None
 
     with design_dir_path.joinpath("data.json").open("w", encoding ="utf-8") as f:
         f.write(design_json)
+    
+    resistance_dir_path = main_dir_path.joinpath("Resistance")
+    resistance_dir_path.mkdir(parents=True, exist_ok=True)
+    print(f"Nested directories '{resistance_dir_path}' created successfully.")
+
+    rCsv: str = "Sample,Point,Resistance (Ohm)\n"
+    design_data: dict = json.loads(design_json)
+    for sample in design_data["samples"]:
+        sample_name: str = sample["name"]
+        for rmp in sample["resistance measurement points"]:
+            rCsv += f"{sample_name},{rmp['name']},\n"
+
+    print(rCsv)
+
+    with resistance_dir_path.joinpath("resistance.csv").open("w", encoding ="utf-8") as f:
+        f.write(rCsv)
 
 def create_design_json(wafer_name: str,
                        wafer_material: str,
                        wafer_size: float,
                        wafer_thickness: str,
                        recipe: str,
-                       sample_names: list,
-                       sample_dimension: str,
-                       sample_center: str,
-                       sample_jopherson_junctions: list,
+                       samples: list,
                        test_junctions: dict
                        ) -> str:
         # Json to have:
@@ -37,12 +151,14 @@ def create_design_json(wafer_name: str,
         # size
         # thickness
         # recipe
-        # sample names 
-        # sample Dimension
-        # sample center
-        #   jj_N style
-        #   jj_N width
-        #   jj_N gap
+        # samples:
+        #   name
+        #   dimension
+        #   center
+        #   jopherson junctions:
+        #       jj_N style
+        #       jj_N width
+        #       jj_N gap
         # test junctions:
         #   type:
         #       DOLAN
@@ -57,10 +173,7 @@ def create_design_json(wafer_name: str,
     rtn['size'] = wafer_size
     rtn['thickness'] = wafer_thickness
     rtn['recipe'] = recipe
-    rtn['sample names'] = sample_names
-    rtn['sample dimension'] = sample_dimension
-    rtn['sample center'] = sample_center
-    rtn['sample jopherson junctions'] = sample_jopherson_junctions
+    rtn['samples'] = samples
     rtn['test junctions'] = test_junctions
 
     return json.dumps(rtn)
